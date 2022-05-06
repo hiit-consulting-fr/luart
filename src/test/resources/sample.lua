b19de1ff1f443a823b68bebec10d205884483a4e
romain.pillot <romain.pillot.ext@inetum.com>
2022-05-06 11:44:51 +0200

    adds lua sample test

---
+++ src/test/resources/sample.lua
@@ -0,0 +1,37 @@
+global = 100
+overrideA = false
+overrideB = false
+overrideC = false
+
+function function_a(test_a, test_b, test_c)
+    print("hello world")
+    global = test_a(1000)
+
+    if test_b == nil then
+        test_c()
+    end
+end
+
+local rootLevelLocalVar = true
+
+function function_b()
+    local test = 1
+
+    local rootLevelLocalVar
+    rootLevelLocalVar = false
+    overrideC = rootLevelLocalVar == false
+
+    local function_a
+    function_a = function() overrideA = true end
+    function_a()
+
+    return test
+end
+
+function_a(function(x) return x end, nil, function() overrideB = true end)
+
+print("global", global == 1000)
+print("function_a", function_a ~= nil)
+print("overrideA", overrideA)
+print("overrideB", overrideB)
+print("overrideC", overrideC)
